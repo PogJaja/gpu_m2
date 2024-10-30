aa2734155bf6fa9e385a31c712ce46af3d79c380
Jarod Acloque <jarod.acloque@live.fr>
2024-10-30 13:49:39 +0100

    tp1_local_coordinates

--- exercices/local_coordinates.py
+++ exercices/local_coordinates.py
@@ -1,17 +1,16 @@
-from numba import cuda
-import numba as nb
 import sys
+
+from numba import cuda
 
 
 @cuda.jit
 def printCoordinates():
     global_id = cuda.grid(1)
-    print("Thread ID x :", cuda.threadIdx.x, "Thread ID y :", cuda.threadIdx.y, "Thread ID z :", cuda.threadIdx.z, "Global ID :", global_id)
+    print("Thread ID x :", cuda.threadIdx.x, "Thread ID y :", cuda.threadIdx.y, "Thread ID z :", cuda.threadIdx.z,
+          "Global ID :", global_id)
 
-def question1():
-    # 1 block of 1 thread
-    threadsPerBlock = 1
-    blocksPerGrid = 1
+
+def executePrintCoordinates(blocksPerGrid, threadsPerBlock):
     print("Starting", sys._getframe().f_code.co_name)
     print("threadsPerBlock ", threadsPerBlock)
     print("blocksPerGrid", blocksPerGrid)
@@ -19,6 +18,11 @@ def question1():
     cuda.synchronize()
 
 
-
-if __name__ == '__main__':
-    question1()
+# 1D Grid, 1 1D block of 1 thread
+executePrintCoordinates(1, 1)
+# 1D Grid, 1 1D block of 16 threads
+executePrintCoordinates(1, 16)
+# 1D Grid, 2 1D block of 1 thread
+executePrintCoordinates(2, 1)
+# 1D Grid, 1 3D block of 16 threads
+executePrintCoordinates(1, (4, 2, 2))
